3b9dedf6aa1bcbf3c2484f9e3e4b73b1b2853ed5
meta-panic <ritaritarita2013@gmail.com>
2025-04-26 01:04:31 +0400

    done cli-related tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,30 @@
+const KEY_PREFIX = "--";
+
 const parseArgs = () => {
-    // Write your code here 
+    const appArgs = getAppArgs(process.argv)
+    printAppArgs(appArgs);
 };
+
+function getAppArgs(args) {
+    const [, , ...appArgs] = args;
+    return appArgs;
+}
+
+function printAppArgs(args) {
+    const result = [];
+    args.forEach((v, index, arr) => {
+        if(!String(v).startsWith(KEY_PREFIX)) {
+            return;
+        }
+
+        const valueIndex = index + 1;
+        if (arr.length >= valueIndex) {
+            result.push(`${v} is ${arr[index + 1]}`)
+        }
+    })
+
+    console.log(result.join(", ").trim());
+}
+
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,26 @@
+const PREFIX = "RSS_";
+
 const parseEnv = () => {
-    // Write your code here 
+   return printEnvs(filterEnvsByPrefix(process.env, PREFIX));
 };
+
+function filterEnvsByPrefix(envObj, prefix) {
+    const filteredEnvs = Object
+        .entries(envObj)
+        .filter(([env,]) => String(env).startsWith(prefix));
+
+    return Object.fromEntries(filteredEnvs);
+}
+
+function printEnvs(envObj) {
+    console.log(Object
+        .entries(envObj)
+        .map(([env, value]) => {
+            return `${env}=${value}`;
+        })
+        .join('; ')
+    );
+}
+
 
 parseEnv();
