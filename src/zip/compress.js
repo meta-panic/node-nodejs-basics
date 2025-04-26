b735930b53e5279926a043f4b4a2fe42fe6d1a99
meta-panic <ritaritarita2013@gmail.com>
2025-04-26 19:08:08 +0400

    feat: add compression and decompression functionality for files using zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,28 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const sourceFilePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const destinationFilePath = path.join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+    try {
+        const readable = createReadStream(sourceFilePath);
+        const gzip = createGzip();
+        const writable = createWriteStream(destinationFilePath);
+
+        await pipeline(readable, gzip, writable);
+
+        console.log('File compressed successfully!');
+    } catch (error) {
+        console.error(`Compression failed: ${error.message}`);
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import { Unzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const destinationFilePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const sourceFilePath = path.join(__dirname, 'files', 'archive.gz');
+
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        const readable = createReadStream(sourceFilePath);
+        const unzip = Unzip();
+        const writable = createWriteStream(destinationFilePath);
+
+        await pipeline(readable, unzip, writable, e => console.log(e));
+
+        console.log('File uncompressed successfully!');
+    } catch (error) {
+        console.error(`Compression failed: ${error.message}`);
+    }
 };
 
 await decompress();
