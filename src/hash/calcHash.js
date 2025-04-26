952c68fb5c87d048a6baf429adc332f80342bb4f
meta-panic <ritaritarita2013@gmail.com>
2025-04-26 18:51:56 +0400

    feat: implement SHA-256 hash calculation for file content

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import { createHash } from "node:crypto";
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+    const readable = createReadStream(filePath, { encoding: "utf8" });
+
+    const hash = createHash("sha256");
+
+    readable.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    readable.on('end', () => {
+        const hexHash = hash.digest("hex");
+        console.log(hexHash);
+    });
 };
+
 
 await calculateHash();
