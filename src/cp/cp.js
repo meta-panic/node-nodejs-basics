ebb2d88d2be195e222b98e352cf0417e742f2dc0
meta-panic <ritaritarita2013@gmail.com>
2025-04-28 00:59:09 +0400

    feat: add child process spawning functionality to execute external scripts

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { spawn } from 'node:child_process';
+import process from 'node:process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [filePath, ...args],
+        { stdio: ['pipe', 'pipe', 'inherit'] }
+    );
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ["someArgument1", "someArgument2"] );
